0b401c7d10b0b47a0359a28c2c23678e51c78c94
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2026-03-30 15:22:30 -0700

    update and streamline `prepInputStack`

--- R/prepInputStack.R
+++ R/prepInputStack.R
@@ -1,11 +1,8 @@
 #' Simple wrapper around `prepInputs` for a stack of raster layers
 #'
-#' @param ... Arguments passed to `fun` (i.e,. user supplied), \code{\link{postProcess}} and
-#'            \code{\link[reproducible]{Cache}}.
-#'            Since `...` is passed to `postProcess`, these will also be passed into the inner
-#'            functions, e.g., \code{\link{cropInputs}}.
+#' @param ... Arguments passed to \code{\link{prepInputs}} and  \code{\link{postProcess}}.
 #'            User should supply several named arguments here, including:
-#'            `targetFile`, `archive`, `url`, `alsoExtract`, `destinationPath`, `fun`, `quick`,
+#'            `targetFile`, `archive`, `url`, `alsoExtract`, `destinationPath`, `to`, `fun`, `quick`,
 #'            `purge`, `overwrite`, and `useCache`.
 #'            See details and examples.
 #'
@@ -14,32 +11,17 @@
 #' @author Tati Micheletti
 #' @export
 #' @importFrom reproducible prepInputs postProcess
-#' @importFrom raster nlayers stack
+#' @importFrom terra rast 
 #'
 #' @rdname prepInputStack
 prepInputStack <- function(...) {
-  dots <- list(...)
   message("prepInput a raster stack...")
-  stackLayers <- reproducible::prepInputs(
-    archive = dots$archive,
-    url = dots$url,
-    targetFile = dots$targetFile,
-    alsoExtract = dots$alsoExtract,
-    destinationPath = dots$destinationPath,
-    fun = "raster::stack"
-  )
-  postProcessedLayers <- lapply(X = seq_len(nlayers(stackLayers)), FUN = function(layer) {
-    lay <- reproducible::postProcess(stackLayers[[layer]],
-      studyArea = dots$studyArea,
-      rasterToMatch = dots$rasterToMatch,
-      destinationPath = dots$destinationPath,
-      filename2 = dots$filename2
-    )
-    names(lay) <- names(stackLayers[[layer]])
-    return(lay)
-  })
-  postProcessedLayers <- raster::stack(postProcessedLayers)
+  stackLayers <- prepInputs(...)
+  postProcessedLayers <- lapply(stackLayers, 
+                                FUN = function(layer, ...) {
+                                  postProcess(layer,...)
+                                }, ...)
+  postProcessedLayers <- rast(postProcessedLayers)
   names(postProcessedLayers) <- names(stackLayers)
-
   return(postProcessedLayers)
 }
